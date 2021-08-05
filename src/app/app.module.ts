29a01b506a18a9f6818d985e36f045ac5466b2b8
yahya lbarrah <yahya.lbarrag@gmail.com>
2021-08-05 12:02:13 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitreComponent } from './components/titre/titre.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitreComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
